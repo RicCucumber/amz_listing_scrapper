ddee52d58a80cc2b2a96f9ebae3d5d15c0203c2a
RicCucumber <rikcucumber@gmail.com>
2019-09-03 02:05:26 +0300

    Major Update added GoogleSheet class added check_rules() method

--- amazon_listing_scraper.py
+++ amazon_listing_scraper.py
@@ -3,6 +3,7 @@ import lxml.html
 import time
 from datetime import datetime
 import configparser
+import re
 
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -12,15 +13,17 @@ import urllib.parse
 
 from scraper.scraper import SeleniumScraper
 from slack.slack import Slack
+from googlesheet.gsheet import GoogleSheet
 
 
-class AmazonBotListing(SeleniumScraper, Slack):
+
+class AmazonBotListing(SeleniumScraper, Slack, GoogleSheet):
 
     fields = [
         'asin',
         'page_status',
-        'product_title',
-        'product_title_indexing',
+        'title',
+        'title_indexing',
         'images_string',
         'bullets_string',
         'bullets_indexing',
@@ -32,24 +35,79 @@ class AmazonBotListing(SeleniumScraper, Slack):
         'review_quantity',
         'rating',
         'price',
-        'stars_string',
+        'top_reviews_rating',
         'report_date'
     ]
 
     parse_result = namedtuple('parse_result', field_names=fields, defaults=['']*len(fields))
 
 
-    def __init__(self, headless=False, slack_user=''):
+    def __init__(self, headless=False, slack_user='', gs_token_path=''):
+        #Inheritance __init__ from parents classes
         SeleniumScraper.__init__(self, headless)
         Slack.__init__(self, slack_user)
+        GoogleSheet.__init__(self, gs_token_path)
 
         self.config = configparser.ConfigParser()
         self.config.read(r'D:\github\amz_listing_scrapper_test\main.ini')
+
+        #Slack info
         self.slack_channel = self.config['slack']['channel']
+
+        #GoogleSheet info
+        self.gs_spreadsheet_id = self.config['googlesheet']['spreadsheet_id']
+        self.gs_main_sheet = self.config['googlesheet']['main_sheet']
+        self.gs_master_data_sheet = self.config['googlesheet']['master_data_sheet']
+        self.gs_rules_sheet = self.config['googlesheet']['rule_sheet']
+
+        #Scrapping results from main sheet
+        self.main_data = self.googlesheet_values(SPREADSHEET_ID=self.gs_spreadsheet_id, RANGE_NAME=self.gs_main_sheet).get('values', [])
+        self.main_sheet_last_row = len(self.main_data)
+        #Get last scrapping result
+        #self.last_data = [row for row in self.main_data[:] if row[17] == self.main_data[-1][17]] - Takes more time on biggest data
+        if self.main_data:
+            self.main_data.reverse()
+            for i in range(0, len(self.main_data)):
+                if self.main_data[i][17] != self.main_data[0][17]:
+                    break
+            self.last_data = self.main_data[:i]
+        else:
+            self.last_data = []
+
+
+        #Master data
+        self.master_data = self.googlesheet_values(SPREADSHEET_ID=self.gs_spreadsheet_id, RANGE_NAME=self.gs_master_data_sheet)
+        #Generate dict -> {columns name: index}
+        self.md_columns = {name: self.master_data['values'][0].index(name) for name in self.master_data['values'][0]}
+        #get clear master data list
+        self.master_data = self.master_data['values'][1:]
+        #Generate dict -> {ASIN: Brand}
+        self.asin_brand_dict = {row[self.md_columns['ASIN']]: row[self.md_columns['Brand']] for row in self.master_data}
+
+        def exception_list(column_name):
+            #Function to get exceptions list by column name in master data sheet
+            #Condition len(row) >= (self.md_columns[column_name] + 1) - means rows that contain values in certain exceptin columns
+            #in this case len of the row should be equal or more than (index + 1) value
+
+            if column_name not in self.md_columns:
+                return []
+
+            return [row[self.md_columns[column_name]] for row in self.master_data if len(row) >= (self.md_columns[column_name] + 1)]
+
+        #Get all exceptions lists
+        self.asins_skip_rating_check = exception_list('Raiting - ASINs do not check')
+        self.bullets_exception = exception_list('Bullets  indexing - do not check')
+        self.asins_skip_reviews_check = exception_list('Reviews - do not check')
+        self.asins_skip_title_check = exception_list('Titles - do not check')
+        self.asins_skip_description_indexing = exception_list('Description  indexing - do not check')
+        self.inactive_asins = exception_list('Inactive')
+
+        #Notification rules data
+        self.rules_data = self.googlesheet_values(SPREADSHEET_ID=self.gs_spreadsheet_id, RANGE_NAME=self.gs_rules_sheet)
+        self.rules_dict = {row[0]:{'period': row[2], 'message': row[3], 'tag': row[4]} for row in self.rules_data['values'][1:]}
 
         self.main_url = 'https://www.amazon.com'
         self.report_date = datetime.now()
-        self.exception_bullets_list = []
 
 
     def scrape_listing(self, asin):
@@ -144,7 +202,7 @@ class AmazonBotListing(SeleniumScraper, Slack):
         #REVIEW BLOCK
         review_block = page_to_parse.xpath('//div[@id="reviewsMedley"]')[0]
         #review quantity
-        review_quantity = review_block.xpath('.//h2[@data-hook="total-review-count"]/text()')[0].split(' ')[0]
+        review_quantity = review_block.xpath('.//h2[@data-hook="total-review-count"]/text()')[0].split(' ')[0].replace(',', '')
         #rating
         rating = review_block.xpath('.//span[@data-hook="rating-out-of-text"]/text()')[0].split(' ')[0]
 
@@ -178,7 +236,7 @@ class AmazonBotListing(SeleniumScraper, Slack):
 
         #Check bullet indexing
         bullets_index_list = [self.__lxml_check_indexing(each)
-            if each and each not in self.exception_bullets_list
+            if each and each not in self.bullets_exception
             else
             ''
             for each in bullets_list
@@ -232,10 +290,11 @@ class AmazonBotListing(SeleniumScraper, Slack):
 
         return response
 
+
     def slack_send_message(self, message_text):
         query = 'chat.postMessage'
         params = {
-            channel': self.slack_channel,
+            channel: self.slack_channel,
             'text': message_text,
             'as_user': False,
             'username': 'Listing_checker',
@@ -245,10 +304,70 @@ class AmazonBotListing(SeleniumScraper, Slack):
         return self.slack_api_call(api_call=query, _type='post', params=params)
 
 
+    def check_rules(self, parsed_result):
+        #Method to check if there is any issue with listing based on rules
+        def images_compare(master_data_images, parsed_result_images):
+            #TODO - add description comment
+            #TODO - try to modify both list in more pythonic way
+            master_data_images = master_data_images.split('\n')
+            parsed_result_images = parsed_result_images.split('\n')
+
+            PATTERN = 'I/(.+?)\.'
+            master_data_images = [re.search(pattern=PATTERN, string=each).group(1) for each in master_data_images]
+            parsed_result_images = [re.search(pattern=PATTERN, string=each).group(1) for each in parsed_result_images]
+
+            for image_id in parsed_result_images:
+                if image_id not in master_data_images:
+                    return True
+
+            return False
+
+
+        def top_review_reduced(parsed_result_stars):
+            parsed_result_stars = parsed_result_stars.split('/')
+            for rating in parsed_result_stars:
+                if float(rating) < 4.0:
+                    return True
+
+            return False
+
+        #TODO - more efficient way to get rows by asin value???
+        master_data_row = []
+        for row in self.master_data:
+            if row[1] == parsed_result.asin:
+                master_data_row = row
+                break
+
+        last_data_row = []
+        for row in self.last_data:
+            if row[0] == parsed_result.asin:
+                last_data_row = row
+                break
+
+        rules_issue = {
+            'PAGE_STATUS': parsed_result.asin == 'Not exist',
+            'PRODUCT_TITLE': parsed_result.title != last_data_row[2],
+            'IMAGES': images_compare(master_data_row[5], parsed_result.images_string),
+            'BULLETS': parsed_result.bullets_string != master_data_row[4],
+            'EBC/DESCRIPTION': not parsed_result.ebc_string and not parsed_result.description,
+            'TITLE_INDEX': parsed_result.title_indexing == 'problem',
+            'BULLETS_INDEX': 'problem' in parsed_result.bullets_indexing,
+            'DESCRIPTION_INDEX': parsed_result.description_indexing == 'problem',
+            'CATEGORY': parsed_result.category != master_data_row[6],
+            'BUYBOX': parsed_result.buybox == 'False',
+            'PRICE': parsed_result.price != last_data_row[14],
+            'REVIEW_QUANTITY': parsed_result.review_quantity < last_data_row[12].replace(',', ''),
+            'RATING_TOTAL': float(parsed_result.rating) < 3.8,
+            'RATING_DECREASE': float(parsed_result.rating) < float(last_data_row[13]),
+            'REVIEW_TOP': top_review_reduced(parsed_result.top_reviews_rating)
+        }
+
+        return rules_issue
+
 
 a = AmazonBotListing(headless=False, slack_user='main')
-# with a as bot:
-#     asin = 'B07MSLMX1J'
-#     data = bot.scrape_listing(asin=asin)
-# print(data)
-print(a.slack_message_history())
+with a as bot:
+    asin = 'B06X91Y91V'
+    data = bot.scrape_listing(asin=asin)
+    print(a.check_rules(data))
+    a.googlesheet_update(SPREADSHEET_ID=a.gs_spreadsheet_id, RANGE_NAME=f'main_data!A{a.main_sheet_last_row+2}', values=[tuple(data)])
